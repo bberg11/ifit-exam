bebc53ba482f020b388408d2c33a4ecde8865a15
Brian Berg <bberg11@gmail.com>
2020-06-28 17:41:49 -0400

    Add slider functionality

--- src/components/MediaSlider/MediaSlider.component.js
+++ src/components/MediaSlider/MediaSlider.component.js
@@ -1,103 +1,134 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useState, useEffect } from 'react';
 
+import IconChevronLeft from '../Icons/IconChevronLeft.component';
+import IconChevronRight from '../Icons/IconChevronRight.component';
 import Card from '../Card/Card.component';
 import wiredLogo from '../../assets/images/wired-logo.svg';
 import mashableLogo from '../../assets/images/mashable-logo.svg';
 import gearJunkieLogo from '../../assets/images/gear-junkie-logo.svg';
 import './MediaSlider.styles.scss';
 
+const data = [
+  {
+    url: '/#',
+    image: gearJunkieLogo,
+    imageAlt: 'Gear Junkie Logo',
+    copy:
+      '“You focus on putting in the work, and the technology handles the rest.”',
+  },
+  {
+    url: '/#',
+    image: wiredLogo,
+    imageAlt: 'Wired Logo',
+    copy: '“Literally transports you from home to wherever you choose to run.”',
+  },
+  {
+    url: '/#',
+    image: mashableLogo,
+    imageAlt: 'Mashable Logo',
+    copy: '“Breathes new life into a tired, old running routine.”',
+  },
+  {
+    url: '/#',
+    image: gearJunkieLogo,
+    imageAlt: 'Gear Junkie Logo',
+    copy:
+      '“COPY - You focus on putting in the work, and the technology handles the rest.”',
+  },
+  {
+    url: '/#',
+    image: wiredLogo,
+    imageAlt: 'Wired Logo',
+    copy:
+      '“COPY - Literally transports you from home to wherever you choose to run.”',
+  },
+  {
+    url: '/#',
+    image: mashableLogo,
+    imageAlt: 'Mashable Logo',
+    copy: '“COPY - Breathes new life into a tired, old running routine.”',
+  },
+];
+
 const MediaSlider = () => {
+  const [currentPosition, setCurrentPosition] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setCurrentPosition(0);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const showPreviousButton = () => {
+    return currentPosition > 0;
+  };
+
+  const showNextButton = () => {
+    return width < 768
+      ? currentPosition < data.length - 1
+      : currentPosition < data.length - 3;
+  };
+
+  const translateValue = () => {
+    if (width < 768) {
+      return `-${100 * currentPosition}%`;
+    }
+
+    if (width < 1024) {
+      return `-${50 * currentPosition}%`;
+    }
+
+    return `-${33.33 * currentPosition}%`;
+  };
+
   return (
     <section className="media-slider">
-      <div className="media-slider__slide">
-        <Card>
-          <a href="/#" className="media-slider__content">
-            <img
-              src={gearJunkieLogo}
-              alt="Gear Junkie Logo"
-              className="media-slider__logo"
-            />
-            <p className="media-slider__copy">
-              “You focus on putting in the work, and the technology handles the
-              rest.”
-            </p>
-          </a>
-        </Card>
+      {showPreviousButton() && (
+        <button
+          type="button"
+          className="media-slider__button media-slider__button--previous"
+          onClick={() => setCurrentPosition(currentPosition - 1)}
+        >
+          <IconChevronLeft />
+        </button>
+      )}
+      <div
+        className="media-slider__slides"
+        style={{ transform: `translateX(${translateValue()})` }}
+      >
+        {data.map((slide, index) => (
+          <div className="media-slider__slide" key={index}>
+            <Card>
+              <a href={slide.url} className="media-slider__content">
+                <img
+                  src={slide.image}
+                  alt={slide.imageAlt}
+                  className="media-slider__logo"
+                />
+                <p className="media-slider__copy">{slide.copy}</p>
+              </a>
+            </Card>
+          </div>
+        ))}
       </div>
-      <div className="media-slider__slide">
-        <Card>
-          <a href="/#" className="media-slider__content">
-            <img
-              src={wiredLogo}
-              alt="Wired Logo"
-              className="media-slider__logo"
-            />
-            <p className="media-slider__copy">
-              “Literally transports you from home to wherever you choose to
-              run.”
-            </p>
-          </a>
-        </Card>
-      </div>
-      <div className="media-slider__slide">
-        <Card>
-          <a href="/#" className="media-slider__content">
-            <img
-              src={mashableLogo}
-              alt="Mashable Logo"
-              className="media-slider__logo"
-            />
-            <p className="media-slider__copy">
-              “Breathes new life into a tired, old running routine.”
-            </p>
-          </a>
-        </Card>
-      </div>
-
-      <div className="media-slider__slide">
-        <Card>
-          <a href="/#" className="media-slider__content">
-            <img
-              src={gearJunkieLogo}
-              alt="Gear Junkie Logo"
-              className="media-slider__logo"
-            />
-            <p className="media-slider__copy">
-              “COPY - You focus on putting in the work, and the technology
-              handles the rest.”
-            </p>
-          </a>
-        </Card>
-      </div>
-      <div className="media-slider__slide">
-        <Card>
-          <a href="/#" className="media-slider__content">
-            <img
-              src={wiredLogo}
-              alt="Wired Logo"
-              className="media-slider__logo"
-            />
-            <p className="media-slider__copy">
-              “COPY - Literally transports you from home to wherever you choose
-              to run.”
-            </p>
-          </a>
-        </Card>
-      </div>
-      <div className="media-slider__slide">
-        <Card>
-          <a href="/#" className="media-slider__content">
-            <img
-              src={mashableLogo}
-              alt="Mashable Logo"
-              className="media-slider__logo"
-            />
-            <p className="media-slider__copy">
-              “COPY - Breathes new life into a tired, old running routine.”
-            </p>
-          </a>
-        </Card>
-      </div>
+      {showNextButton() && (
+        <button
+          type="button"
+          className="media-slider__button media-slider__button--next"
+          onClick={() => setCurrentPosition(currentPosition + 1)}
+        >
+          <IconChevronRight />
+        </button>
+      )}
     </section>
   );
 };
