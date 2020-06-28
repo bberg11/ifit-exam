a7e420d6fe521d0a7c41e7f998892b761b77de15
Brian Berg <bberg11@gmail.com>
2020-06-28 14:09:16 -0400

    Fade in hero text

--- src/components/Hero/Hero.component.js
+++ src/components/Hero/Hero.component.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 import Button from '../Button/Button.component';
 import heroImage from '../../assets/images/hero.png';
 import './Hero.styles.scss';
 
 const Hero = () => {
+  const [textIsVisible, setTextIsVisible] = useState(false);
+
+  useEffect(() => {
+    window.setTimeout(() => {
+      setTextIsVisible(true);
+    }, 500);
+  }, []);
+
   return (
     <section className="hero">
       <div
@@ -12,7 +21,12 @@ const Hero = () => {
         style={{ backgroundImage: `url(${heroImage})` }}
       />
       <div className="hero__content">
-        <h1 className="hero__heading">
+        <h1
+          className={classNames({
+            hero__heading: 'true',
+            'hero__heading--is-visible': textIsVisible,
+          })}
+        >
           The best personal training, right in your own home
         </h1>
         <div className="hero__action">
